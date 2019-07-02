aa7c7c2d91feae4a0c111405eab5a72114a2fb09
nguyen dang <dangnguyenminh171@gmail.com>
2019-07-02 13:20:22 +0700

    commit1 test

--- testgit/ViewController.swift
+++ testgit/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        print("nguyendang171")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
