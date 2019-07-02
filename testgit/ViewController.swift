c41808ed5269d7c0a6e8280cb903b4e31e5bf9e5
nguyen dang <dangnguyenminh171@gmail.com>
2019-07-02 13:11:59 +0700

    Initial Commit

---
+++ testgit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testgit
+//
+//  Created by Dang Minh Nguyen on 7/2/19.
+//  Copyright © 2019 dang ming nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
